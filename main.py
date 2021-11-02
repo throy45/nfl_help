d3edbc76d4aa0a0030b0f8c52f4bf0a05e360b8b
throy45 <thomas.roy.2@ulaval.ca>
2021-11-02 15:52:37 -0400

    Still not fully working but slowly getting there. The GUI is starting to look half decent and the next step will be to display the games of the week on it, then it will be to fetch the individual games stats and give a score to each game.

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 from nfl_calendar import Calendar
+from GUI import Display
 
 # This program aims to tell the user which NFL game from the past week would be the most interesting to watch.
 # The link is not yet to nfl.com/stats since I'm still in the process of learning parsing.
 
 
 def main():
+    calendar = Calendar(6)
 
-    # We ask the user if he wants to update the html file because too much of it will DDOS the site.
-    html_file.update_html()
-
-    # Open the file
-    html = html_file.read_html()
-
-    """
-    soup = BeautifulSoup(html, "html.parser")
-
-    rows = soup.select('div.game_summaries:nth-child(4)')
-    print(rows)
-    """
+    dis = Display(calendar)
+    dis.run()
 
 
-# main()
-calendar = Calendar(7)
+
+main()
 
 
