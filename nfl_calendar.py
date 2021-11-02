e93f2b813dfcb353a21b9205a966fa359fd630b9
throy45 <thomas.roy.2@ulaval.ca>
2021-11-02 15:53:06 -0400

    Added a bunch of comments to hopefully make if clearer.

--- nfl_calendar.py
+++ nfl_calendar.py
@@ -13,7 +13,8 @@ class Calendar:
         """
         Creates two dictionnaries, self.weeks and self.games.
         self.weeks : Stores each html week file
-        self.games : Stores date and teams for each games in a 2D dict. structure : self.games[week_number][game_name]
+        self.games : Stores date and teams for each games in a 2D dict.
+                     structure : self.games[week_number][game_name]
 
         :param current_week: int
         """
@@ -22,25 +23,29 @@ class Calendar:
         self.games = {}
         for week_index in range(18):
             week_nb = week_index + 1
-            url = "https://www.pro-football-reference.com/years/2021/week_" + str(week_nb) + ".htm"
+            url = "https://www.pro-football-reference.com/years/2021/week_" + \
+                  str(week_nb) + ".htm"
             file_name = "week_" + str(week_nb)
             week_html = HTML(url, file_name)
             self.weeks[week_nb] = week_html.read_html()
             self.games[week_nb] = {}
 
-        # Add games to self.games dict (so add the date and the teams.
-        # since we don't have the info for the current week we need to do until last week.
+        # Add games to self.games dict (so add the date and the teams. since
+        # we don't have the info for the current week we need to do until
+        # last week.
         for week_index in range(current_week - 1):
-            self.update_week_games(week_index+1)
+            self.update_week_games(week_index + 1)
 
     def update_week_file(self, week_nb):
         """
-        Retrieves the games happening on week week_nb and creates/updates the html file.
+        Retrieves the games happening on week week_nb and creates/updates the
+        html file.
         :param week_nb: integer (between 1 and 18)
         :return:
         """
         week_nb = str(week_nb)
-        url = "https://www.pro-football-reference.com/years/2021/week_" + week_nb + ".htm"
+        url = "https://www.pro-football-reference.com/years/2021/week_" + \
+              week_nb + ".htm"
         file_name = "week_" + week_nb
 
         html_file = HTML(url, file_name)
@@ -59,29 +64,33 @@ class Calendar:
 
     def update_week_games(self, week_nb):  # ######## ici
         """
-        Adds or updates the games for specified week in the games dict. Currently can only fetch past games.
+        Adds or updates the games for specified week in the games dict.
+        Currently can only fetch past games.
         :param week_nb: int
         :return:
         """
-        url = "https://www.pro-football-reference.com/years/2021/week_" + str(week_nb) + ".htm"
+        url = "https://www.pro-football-reference.com/years/2021/week_" + str(
+                week_nb) + ".htm"
         file_name = "week_" + str(week_nb)
 
         # Creates HTML instance to be able to retrieve it with read_html method
         html_file = HTML(url, file_name)
         html = html_file.read_html()
 
-        # uses Beautiful Soup to parse the html and find the section corresponding to
-        # the match
+        # uses Beautiful Soup to parse the html and find the section
+        # corresponding to the match
         soup = BeautifulSoup(html, "html.parser")
         rows = soup.select('.game_summary')
 
-        # for every game (16) per week, create an element for that game in self.games
+        # for every game (16) per week, create an element for that game in
+        # self.games
         for game_index in range(16):
             game_name = f"Game {str(game_index + 1)}"
 
             # select the date of the particular game
             date = rows[game_index].select_one('.date td').text.strip()
-            # convert it to datetime.date element to be able to compare with today's date
+            # convert it to datetime.date element to be able to compare with
+            # today's date
             date = datetime.datetime.strptime(date, "%b %d, %Y").date()
 
             # select the winner's and the loser's name
