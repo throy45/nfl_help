2bcf7fa4579e87b65a2a6b0043db4df3d13e5739
throy45 <thomas.roy.2@ulaval.ca>
2021-11-09 19:30:49 -0500

    Updated comments

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from nfl_calendar import Calendar
 from GUI import Display
 
-# This program aims to tell the user which NFL game from the past week would be the most interesting to watch.
-# The link is not yet to nfl.com/stats since I'm still in the process of learning parsing.
+# First this programs gives the users the NFL games from the week he selects.
+# Then it aims to tell the user which NFL game from the past week would
+# be the most interesting to watch (not yet achieved).
 
 
 def main():
-    calendar = Calendar(6)
+    calendar = Calendar(9)
 
     dis = Display(calendar)
     dis.run()
